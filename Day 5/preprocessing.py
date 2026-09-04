31727bd7c0d0c37c34a3dd990e04ff894352b853
ABDUL BASIT SHEHZAD <basit12876@gmail.com>
2026-09-04 17:36:52 +0500

    Week 7 Day 5

---
+++ Day 5/preprocessing.py
@@ -0,0 +1,13 @@
+import re
+
+
+def preprocess_text(text):
+    text = text.strip()
+
+    text = text.lower()
+
+    text = re.sub(r"http\S+|www\S+", "", text)
+
+    text = re.sub(r"\s+", " ", text)
+
+    return text.strip()
